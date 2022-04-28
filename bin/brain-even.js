5482cf855ea8eb221959e45aa35b645639fe19ff
Yulia <julia3694510@gmail.com>
2022-04-28 21:27:40 +0300

    games: parity check

---
+++ bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { parityCheck } from '../src/cli-even.js';
+
+parityCheck();
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,7 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!");
-export const userName = readlineSync.question("May I have your name?: ");
+export const greetings = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${userName}!`);
+};
